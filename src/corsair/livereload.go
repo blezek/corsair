385a760ad8eddd0e83f4f28ba1e922cd28aa95a0
Daniel Blezek <blezek.daniel@mayo.edu>
2015-01-22 06:05:18 -0600

    Options changes

--- src/corsair/livereload.go
+++ src/corsair/livereload.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"github.com/dblezek/lrserver"
 	"github.com/go-fsnotify/fsnotify"
 	"log"
+	"os"
 	"time"
 )
 
 var lastReloadRequest time.Time = time.Now()
+var readInput = flag.Bool("read", false, "Read standard input to get file changes, helpful when using fswatch")
+var readNull = flag.Bool("null", false, "Read looking for nulls (default is new line)")
+
+var debounceTimeout = flag.Int("timeout", 300, "Time in milliseconds to wait for repeated file change events.  Typically programs like fswatch generate 3 file events per file change, it is good to wait a few milliseconds before sending the livereload request to prevent multiple, unwanted reloads.")
 
 // Take a request, sleep on it.  If we were the last request, then
 // tell the lrserver to reload, otherwise just go away.
@@ -26,6 +33,23 @@ func livereloader(directory string) {
 
 	log.Printf("Starting livereloader @ %v\n", directory)
 	watcher, err := fsnotify.NewWatcher()
+
+	if *readInput {
+		var terminationCharacter byte = byte('\n')
+		if *readNull {
+			terminationCharacter = 0
+		}
+
+		go func() {
+			// Read from stdin and request a reload
+			reader := bufio.NewReader(os.Stdin)
+			for {
+				f, _ := reader.ReadString(terminationCharacter)
+				log.Printf("Read %v\n", f)
+				go request(f, 300*time.Millisecond)
+			}
+		}()
+	}
 
 	go func() {
 		for {
